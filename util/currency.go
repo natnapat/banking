63dd2aa7190dad5d1c98bf6ac6a0c872aecc86fe
Natnapat Keeratikornpisut <natnapat@IceMBP.local>
2022-08-18 01:53:31 +0700

    change some config

--- util/currency.go
+++ util/currency.go
@@ -5,12 +5,13 @@ const (
 	USD = "USD"
 	EUR = "EUR"
 	CAD = "CAD"
+	JPY = "JPY"
 )
 
 // IsSupportedCurrency returns true if the currency is supported
 func IsSupportedCurrency(currency string) bool {
 	switch currency {
-	case USD, EUR, CAD:
+	case USD, EUR, CAD, JPY:
 		return true
 	}
 	return false
